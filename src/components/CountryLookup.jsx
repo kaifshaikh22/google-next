4487d1808e29c3e1ca930f326533f8a88c71d734
kaif shaikh <kaaifshaikh22@gmail.com>
2024-03-24 16:22:52 +0530

    add footer component

---
+++ src/components/CountryLookup.jsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function CountryLookup() {
+    const [country, setCountry] = useState('united States');
+
+    useEffect(() => {
+        const getCountry = async () => {
+            const response = await fetch(
+                `https://extreme-ip-lookup.com/json/?key=${process.env.NEXT_PUBLIC_IP_API_KEY}`
+                )
+              .then((res) => res.json())
+              .then((data) => data.country);
+              if (!response) return;
+              setCountry(response);
+        };
+        getCountry();
+    }, []);
+  return (
+    <div>{country}</div>
+  )
+}
