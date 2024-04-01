4fb0e24c62d4bb453e04a655a8204d72fa09cf38
kaif shaikh <kaaifshaikh22@gmail.com>
2024-04-01 14:15:09 +0530

    updated pages

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -4,14 +4,13 @@ import Link from 'next/link';
 
 export default async function ImageSearchPage({ searchParams }) {
   const startIndex = searchParams.start || '1';
-  await new Promise((resolve) => setTimeout(resolve, 10000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}'}&searchType=image&start=${startIndex}`
   );
   if (!response.ok) throw new Error('Something went wrong');
   const data = await response.json();
   const results = data.items;
-
   if (!results) {
     return (
       <div className='flex flex-col justify-center items-center pt-10'>
@@ -27,6 +26,5 @@ export default async function ImageSearchPage({ searchParams }) {
       </div>
     );
   }
-
   return <div>{results && <ImageSearchResults results={data} />}</div>;
 }
